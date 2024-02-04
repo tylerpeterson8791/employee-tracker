e45eb0f3569466ee71c442a9dedbcbd595ff2041
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-02-04 16:53:52 -0600

    deleteRole built, tested, working.  Pushing branch.

--- index.js
+++ index.js
@@ -27,11 +27,6 @@ mainMenu();
 
 
 /*
-Delete departments, roles.
-IF i delete a role that is assigned to an employee what happens?  Same with department?  What if I delete an employee who is a manager?
-I ADDED IN USER MESSAGING ON CONSOLE LOG
-
-
 View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
 
 Maybe import queries (especially view employee) instead of consting them in?

697aa0e23c78f04663dfad39e1096dea3860ab6c
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-02-04 02:40:13 -0600

    Laid out plan for tomorrow commented out in index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const inquirer = require("inquirer");
-const mysql = require("mysql2");
 const mainMenu = require('./lib/mainMenu.js');
-// const db = require("../config/connect");
-//don't know if this is just good here or needs to be in all the functions?  Leaning towards all the functions...
+
+//console.log some ASCII ART here for the intro
 
 mainMenu();
 
@@ -10,33 +8,12 @@ mainMenu();
 
 
 
-/*GIVEN a command-line application that accepts user input
-WHEN I start the application
-THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-
-WHEN I choose to view all departments
-THEN I am presented with a formatted table showing department names and department ids
-
+/*
 WHEN I choose to view all roles
-THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+THEN I am presented with the job title, ******role id*****ADD*****, the department that role belongs to, and the salary for that role
 
-WHEN I choose to view all employees
-THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+FIX EXIT APPLICATION.  Don't know how to do that???
 
-WHEN I choose to add a department
-THEN I am prompted to enter the name of the department and that department is added to the database
-
-WHEN I choose to add a role
-THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-
-WHEN I choose to add an employee
-THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-
-WHEN I choose to update an employee role
-THEN I am prompted to select an employee to update and their new role and this information is updated in the database
-Mock-Up*/
-
-/*Bonus
 Try to add some additional functionality to your application, such as the ability to do the following:
 
 Update employee managers.
@@ -47,4 +24,6 @@ View employees by department.
 
 Delete departments, roles, and employees.
 
-View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.*/
+View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
+
+*/

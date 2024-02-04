1a5ed58550b8fe778ef93662c417581c955fb95d
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-02-04 02:17:16 -0600

    Updated viewEmployees query to replace Null.  Acceptance criteria met.  Pushing branch.  Will continue on bonus and cleanup tomorrow.

--- db/query_viewEmployees.sql
+++ db/query_viewEmployees.sql
@@ -5,14 +5,22 @@ SELECT
     role.title AS Title,
     role.salary AS Salary,
     department.name AS Department,
-    CONCAT(manager.first_name, ' ', manager.last_name) AS Supervisor
+    -- CASE is kinda like if/then of sql
+    CASE
+        -- if null then display **MANAGER**
+        WHEN employee.manager_id IS NULL THEN '**MANAGER**'
+        -- or concat together manager's first name and last name as Supervisor END finishes statement
+        ELSE CONCAT(manager.first_name, ' ', manager.last_name)
+    END AS Supervisor
 FROM
     employee
 JOIN
     role ON employee.role_id = role.id
 JOIN
     department ON role.department_id = department.id
+-- have to use LEFT JOIN because it's merging with NULL values
 LEFT JOIN
+    --the manager of the employee is where the employees manager_id matches the id of the manager
     employee manager ON employee.manager_id = manager.id;
 
     -- this is getting a little unruly in the one line.

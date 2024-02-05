6a94c79535eef6d13cc3fbe5a6e8f7e09a7bf96f
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-02-04 19:43:44 -0600

    viewDepartmentPayroll complete.

--- index.js
+++ index.js
@@ -21,14 +21,3 @@ console.log(`
 
 
 mainMenu();
-
-
-
-
-
-/*
-View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
-
-Maybe import queries (especially view employee) instead of consting them in?
-
-*/
